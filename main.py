34f6452bc6f3c4154874c31d90e86c4eec987db7
haijin cheng <chjtxwd@vip.qq.com>
2023-11-09 04:53:20 +0000

    write to json file

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-
 url = 'https://flaresolverr.haijin666.top/v1'
 headers = {
     'Content-Type': 'application/json'
@@ -21,9 +20,22 @@ response = json.loads(response)
 html = response['solution']['response']
 soup = BeautifulSoup(html, 'html.parser')
 
+# 创建一个空的列表，用于存储视频信息
+videos = []
+
 # 查找所有ID为"video-title"的元素，并获取它们的href属性值
 video_title_elements = soup.find_all(id='video-title')
 for video_title_element in video_title_elements:
-    # 获取href属性的值
+    # 获取文本和href属性的值
+    text = video_title_element.text
     href_value = video_title_element.parent['href']  # Assuming the parent element contains the href attribute
-    print(f"Text: {video_title_element.text}, href: {href_value}")
+    # 将信息封装成JSON对象
+    video_info = {"text": text, "href": href_value}
+    # 添加到列表中
+    videos.append(video_info)
+
+# 将视频信息写入videos.json文件
+with open('videos.json', 'w', encoding='utf-8') as json_file:
+    json.dump(videos, json_file, ensure_ascii=False, indent=4)
+
+print("Videos information has been written to videos.json")
